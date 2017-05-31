88380a47733564977607b35c8e5fdcb44869ee5b
Valery Mendez <valery220995@gmail.com>
2017-05-31 13:52:49 -0600

    Add Lombok to project

--- src/main/java/com/example/demo/entities/Shop.java
+++ src/main/java/com/example/demo/entities/Shop.java
@@ -1,8 +1,15 @@
 package com.example.demo.entities;
 
+import lombok.Data;
+
 /**
  * Created by damz on 5/30/2017.
  */
+@Data
 public class Shop {
+
+    private String shopName;
+    private Integer shopAddressNumber;
+    private Integer shopAddressPostCode;
 
 }
