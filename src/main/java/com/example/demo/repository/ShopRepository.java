635971cc4d550a5b2d17cedb1b99d6f9ef836fa9
Valery Mendez <valery220995@gmail.com>
2017-06-01 15:04:27 -0600

    Update build.gradle, update service and repository

---
+++ src/main/java/com/example/demo/repository/ShopRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.entities.Shop;
+
+/**
+ * Created by damz on 6/1/2017.
+ */
+public interface ShopRepository {
+
+    Number create(Shop shop);
+}
